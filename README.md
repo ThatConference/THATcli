4e1f2fc603baf4c5cef16091b4dd7597f23dc9b1
Clark Sell <clark.sell@csell.net>
2018-05-23 06:41:45 -0500

    inital checkin of this repo, enables a command line to get all big red button devices from api in a table format

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # THATcli
+
+Just another CLI. Very very limited at the moment.
+
+In fact we wouldn't even call it a CLI but one day it will become one.
---
+++ index.js
@@ -0,0 +1,35 @@
+const Table = require("cli-table");
+const request = require("graphql-request").request;
+
+const query = `{
+    devices {
+        id
+        tcId 
+        coreId 
+        roomName 
+        isAssigned 
+        particleName 
+    }
+}`;
+
+const table = new Table({
+  head: ["id", "tcId", "coreId", "roomName", "isAssigned", "particleName"],
+  colWidths: [30, 8, 30, 20, 15, 20]
+});
+
+const url = "https://api.thatconference.com/graphql";
+
+request(url, query).then(data => {
+  const tableGuts = data.devices.map(item => {
+    return [
+      item.id,
+      item.tcId,
+      item.coreId,
+      item.roomName,
+      item.isAssigned,
+      item.particleName
+    ];
+  });
+  table.push(...tableGuts);
+  console.log(table.toString());
+});
